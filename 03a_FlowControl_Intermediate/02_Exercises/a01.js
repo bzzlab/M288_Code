4dcb44d67e6ca941cd5366565bf97ecd124d659c
Daniel Garavaldi <daniel.garavaldi@bzz.ch>
2023-09-21 08:06:17 +0200

    03_ Intro, Exer

---
+++ 03a_FlowControl_Intermediate/02_Exercises/a01.js
@@ -0,0 +1,42 @@
+/*
+Task: Traffic light with argument color
+A. Re-wright traffic light example with color as an argument.
+B. If no argument is provided then traffic light is out of order.
+
+Test:
+$ node a01.js
+Traffic light is undefined
+OUT OF ORDER
+
+$ node a01.js red
+Traffic light is red
+DON'T WALK!
+
+$ node a01.js orange
+Traffic light is orange
+ATTENTION
+
+$ node a01.js green
+Traffic light is green
+WALK
+
+*/
+'use strict';
+//Read (parse) argument
+//??
+//??
+//??
+//Check with if-then-else
+//??
+//??
+//??
+//??
+//??
+//??
+//??
+//??
+//??
+//??
+//??
+//??
+//??
