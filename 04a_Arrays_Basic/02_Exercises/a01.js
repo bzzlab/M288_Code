81e8a2d294a1c754616fc9b5ea7a64f00283f418
Daniel Garavaldi <daniel.garavaldi@bzz.ch>
2023-09-28 07:50:55 +0200

    04a_*

---
+++ 04a_Arrays_Basic/02_Exercises/a01.js
@@ -0,0 +1,20 @@
+/*
+Task: Working with arrays
+Compute and print array values based on comments below.
+ */
+
+//Array of numbers
+let numberArray = [ 8.07,3.51,6.16,4.25,3.39,4.48 ];
+//Print all elements
+//??
+//Print first element
+//??
+//Print array length
+//??
+//Print last element
+//??
+//??
+
+
+
+
