0d53b77a8ab9d69e58eb488e3107dcc5744d0d2a
Daniel Garavaldi <daniel.garavaldi@bzz.ch>
2023-10-26 15:10:09 +0200

    Update 01_loops.js

--- 05a_Loops_Basic/01_Intro/01_loops.js
+++ 05a_Loops_Basic/01_Intro/01_loops.js
@@ -1,23 +1,8 @@
-/*
-Topic: Loops (Schleifen) with arrays
- */
-
-//Example 1 - print array elements with a for-loop
-//declare an array with classes
-let classes = ['IM21a', 'IM22a', 'IM22b','IM23a', 'IM23b'];
-
-//naive way of output
-console.log(classes[0]);
-console.log(classes[1]);
-console.log(classes[2]);
-console.log(classes[3]);
-
-//smart way of output with a for-loop
-for(let i=0; i< classes.length;i++){
-    console.log(classes[i]);
-}
-
-
+'use strict';
+//Array
+let kantone = ['Zürich', 'St. Gallen', 'Graubünden', 'Jura', 'Genf', 'Luzern','Schwyz', '...'];
+//for-loop
+//while-loop
 
 
 
