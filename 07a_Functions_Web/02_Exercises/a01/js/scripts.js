cb5e47cf860e6005946d58e2e1d2249012a71466
Daniel Garavaldi <daniel.garavaldi@gmail.com>
2023-11-09 09:03:10 +0100

    07a_*

---
+++ 07a_Functions_Web/02_Exercises/a01/js/scripts.js
@@ -0,0 +1,63 @@
+//set variables or constant EUR,CHF & USD
+//??
+//??
+//??
+
+//Set conversion i.e. from https://www1.oanda.com/currency/converter/
+//??
+//??
+//??
+//??
+
+//get values from form
+//??
+//??
+//??
+//??
+
+
+/**
+ * function convert amount by exchange rate
+ * @param amount
+ * @param exchange_rate
+ * @returns {string}
+ */
+//??
+//??
+//??
+
+/**
+ *  from CHF to foreign exchange
+ */
+function localToForeign() {
+    /* Ist Ausgangswährung CHF dann ... */
+//??
+        /* ... und die Zielwährung EUR dann ... */
+//??
+            /* ... rufe die Funktion auf zur Berechnung des EUR-Kurses  */
+//??
+//??
+//??
+            /* ... rufe die Funktion auf zur Berechnung des USD-Kurses  */
+//??
+//??
+//??
+}
+
+/**
+ *  from foreign exchange to CHF
+ */
+function foreignToLocal() {
+    /* Ist Ausgangswährung CHF dann ... */
+//??
+        /* ... und die Zielwährung EUR dann ... */
+//??
+            /* ... rufe die Funktion auf zur Berechnung des EUR-Kurses  */
+//??
+//??
+//??
+            /* ... rufe die Funktion auf zur Berechnung des USD-Kurses  */
+//??
+//??
+//??
+}
