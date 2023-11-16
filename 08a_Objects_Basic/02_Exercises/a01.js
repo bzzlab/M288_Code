32b143a7e5ce594568572c70e18d8dc49a06a3cc
Daniel Garavaldi <daniel.garavaldi@gmail.com>
2023-11-16 08:23:25 +0100

    08a_

---
+++ 08a_Objects_Basic/02_Exercises/a01.js
@@ -0,0 +1,49 @@
+/*
+Task: Create an object-literal 'person' with the properties
+Anna Muster, female, born on March 1, 2002?
+In the next step: expand the existing example with a birthday edition
+*/
+
+//constant full months
+//??
+//??
+
+let person2 = {
+    //first name
+//??
+    //last name
+//??
+    //gender
+//??
+    //birthday
+//??
+
+    /**
+     * method get full name
+     * @returns {string}
+     */
+//??
+//??
+//??
+    /**
+     * method get birthday
+     * @returns {string}
+     */
+//??
+//??
+//??
+//??
+//??
+//??
+}
+/*
+Sample output
+weiblich
+2002-03-01T00:00:00.000Z
+Der Name ist Anna Muster.
+Geburtstag am 1. March 2002
+ */
+//??
+//??
+//??
+//??
