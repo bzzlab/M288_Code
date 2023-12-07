80e3d0c7edb7187a990bda99a1dc764a19308cad
Daniel Garavaldi <daniel.garavaldi@gmail.com>
2023-12-07 10:36:07 +0100

    08a_Intro/*

--- 08a_Objects_Basic/01_Intro/ME22c/abmachungen_test.js
+++ 08a_Objects_Basic/01_Intro/ME22c/abmachungen_test.js
@@ -1,20 +1,14 @@
-/*
-//copy by reference
-const {Abmachung} = require("./abmachungen_lib");
-let abmachung2 = abmachung1;
-//copy by value
-let abmachung2 = Object.create(abmachung1);
-abmachung2.freunde = ["Giona", "Loris", "Kareem"];
-abmachung2.was ="Film anschauen";
-abmachung2.wann = new Date('2023-11-25 20:00');
-abmachung2.wo= "Kino Abaton";
-abmachung2.todo = ["Snacks", "Cola","gute Stimmung"];
+let vorlage = require('./abmachungen_class');
 
-console.log(abmachung1);
-console.log(abmachung2);*/
+let abmachungen = [];
 
-let abmachung = require('./abmachungen_class');
-let abmachung1 =
-    new Abmachung('Fussball spielen',
-        new Date('2023-11-24T20:00'), "Hardhof");
-console.log(abmachung1.sendNotification())
+abmachungen.push(new vorlage.Abmachung('Fussball spielen',
+    new Date('2023-11-24T20:00'), "Hardhof"));
+abmachungen.push(new vorlage.Abmachung('Skifahren',
+    new Date('2023-12-27T10:00'), "Lenzerheide-Talstation"));
+abmachungen.push(new vorlage.Abmachung('Silvester',
+    new Date('2023-12-31T22:00'), "Zürich"));
+
+for (let i=0;i<abmachungen.length;i++){
+    console.log(abmachungen[i].sendNotification())
+}
