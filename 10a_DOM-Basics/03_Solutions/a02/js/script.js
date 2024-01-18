3d99a68c72b606506effc483972c6ce80b3fb46e
Daniel Garavaldi <daniel.garavaldi@gmail.com>
2024-01-18 10:46:50 +0100

    a02-sol

--- 10a_DOM-Basics/03_Solutions/a02/js/script.js
+++ 10a_DOM-Basics/03_Solutions/a02/js/script.js
@@ -1,47 +1,44 @@
 //start at index 1
 let index = 1;//??
-//get html elements
+//get pic element
 let bild = document.getElementById("pic");//??
 let label = document.getElementById("label");//??
 let price = document.getElementById("price");//??
-/**
- * Init function for first product
- */
+
 function init(){
-    bild.src = "img/" + productList[index - 1].img;//??
-    label.innerHTML = productList[index - 1].product;//??
-    price.innerHTML = productList[index - 1].price;//??
+    bild.src = "img/" + productList[0].img;
+    label.innerHTML = productList[0].product;
+    price.innerHTML = productList[0].price;
 }
 
 /**
- * move forward function
+ * function forward
  */
-function forward() {
+function forward() {//??
     if (bild != null) {//??
         if (index < 5) {//??
             index++;//??
-        } else if (index === 5) {//??
-            index = 1;//??
-        }//??
-        bild.src = "img/" + productList[index - 1].img;//??
-        label.innerHTML = productList[index - 1].product;//??
-        price.innerHTML = productList[index - 1].price;//??
+        } else if (index === 5){
+            index = 1;
+        }
+        bild.src = "img/" + productList[index-1].img;
+        label.innerHTML = productList[index-1].product;
+        price.innerHTML = productList[index-1].price;
     }//??
-}
+}//??
 
 /**
- * move backward function
+ *  function backward
  */
-function backward() {
+function backward() {//??
     if (bild != null) {//??
         if (1 < index) {//??
             index--;//??
-        }  else if (index === 5) {//??
-            index = 1;//??
-        }//??
-        bild.src = "img/" + productList[index - 1].img;//??
-        label.innerHTML = productList[index - 1].product;//??
-        price.innerHTML = productList[index - 1].price;//??
+        } else if (index === 1){
+            index = 5;
+        }
+        bild.src = "img/" + productList[index-1].img;
+        label.innerHTML = productList[index-1].product;
+        price.innerHTML = productList[index-1].price;
     }//??
-}
-
+}//??
