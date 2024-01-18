cecc717480cae42a199cda97b758442b0680aed0
Daniel Garavaldi <daniel.garavaldi@gmail.com>
2024-01-18 09:02:07 +0100

    a02-sol

---
+++ 10a_DOM-Basics/03_Solutions/a02/js/script.js
@@ -0,0 +1,47 @@
+//start at index 1
+let index = 1;//??
+//get html elements
+let bild = document.getElementById("pic");//??
+let label = document.getElementById("label");//??
+let price = document.getElementById("price");//??
+/**
+ * Init function for first product
+ */
+function init(){
+    bild.src = "img/" + productList[index - 1].img;//??
+    label.innerHTML = productList[index - 1].product;//??
+    price.innerHTML = productList[index - 1].price;//??
+}
+
+/**
+ * move forward function
+ */
+function forward() {
+    if (bild != null) {//??
+        if (index < 5) {//??
+            index++;//??
+        } else if (index === 5) {//??
+            index = 1;//??
+        }//??
+        bild.src = "img/" + productList[index - 1].img;//??
+        label.innerHTML = productList[index - 1].product;//??
+        price.innerHTML = productList[index - 1].price;//??
+    }//??
+}
+
+/**
+ * move backward function
+ */
+function backward() {
+    if (bild != null) {//??
+        if (1 < index) {//??
+            index--;//??
+        }  else if (index === 5) {//??
+            index = 1;//??
+        }//??
+        bild.src = "img/" + productList[index - 1].img;//??
+        label.innerHTML = productList[index - 1].product;//??
+        price.innerHTML = productList[index - 1].price;//??
+    }//??
+}
+
